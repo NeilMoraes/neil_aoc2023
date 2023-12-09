98087b4e6b8b896b28c488b5caec3e139cdb08bf
Neil Moraes <mosraughen@gmail.com>
2023-12-09 13:15:36 +1300

    new solutions

--- day5.py
+++ day5.py
@@ -50,13 +50,64 @@ def get_destination(seed: int, mapping: List[int]):
     return seed
 
 
-# def get_seeds(seeds: List[int]):
-#     """Converts range of seed numbers to list of seeds"""
-#     seed_numbers = []
-#     for start, end in zip(range(0, len(seeds), 2), range(2, len(seeds) + 1, 2)):
-#         start_seed, length = seeds[start:end]
-#         seed_numbers.extend(list(range(start_seed, start_seed + length)))
-#     return set(seed_numbers)
+def remove_overlap(range1, range2):
+    range1_start = range1[0]
+    range1_end = range1[0] + range1[1] - 1
+    range2_start = range2[0]
+    range2_end = range2[0] + range2[1] - 1
+
+    issubset = (range2_start >= range1_start) & (range2_end <= range1_end)
+    issuperset = (range2_start <= range1_start) & (range2_end >= range1_end)
+    isoverlap1 = (range2_start >= range1_start) & (range2_start <= range1_end)
+    isoverlap2 = (range1_start >= range2_start) & (range1_start <= range2_end)
+
+    # range2 is subset of range1
+    if issubset:
+        return []
+    elif issuperset:
+        return [
+            range2_start,
+            range1_start - range2_start,
+            range1_end,
+            range2_end - range1_end,
+        ]
+    # partial overlap
+    elif isoverlap1:
+        return [range1_end, range2_end - range1_end]
+    elif isoverlap2:
+        return [range2_start, range1_start - range2_start]
+    # no overlap
+    else:
+        return range2
+
+
+def flatten(l: List[List]) -> List:
+    """Flattens nested list"""
+    return [y for x in l for y in x]
+
+
+def dedupe_seed_range(seed_ranges: List[int]):
+    """Remove duplicates in seed ranges for Q2 to speed up computation?"""
+    deduped_seed_ranges = []
+    for start, end in zip(
+        range(0, len(seed_ranges), 2), range(2, len(seed_ranges) + 1, 2)
+    ):
+        current_seed_range = seed_ranges[start:end]
+        if not deduped_seed_ranges:
+            deduped_seed_ranges.append(current_seed_range)
+        else:
+            new_range = current_seed_range
+            for i, rng in enumerate(deduped_seed_ranges):
+                if (
+                    len(remove_overlap(rng, new_range)) > 2
+                ):  # happens when range2 is superset of range1
+                    deduped_seed_ranges[i - 1] = new_range
+                else:
+                    new_range = remove_overlap(rng, new_range)
+            if new_range:
+                deduped_seed_ranges.append(new_range)
+
+    return flatten(deduped_seed_ranges)
 
 
 if __name__ == "__main__":
@@ -78,7 +129,7 @@ if __name__ == "__main__":
             lowest_location = location
     print("Q1:", lowest_location)
 
-    # Q2 - get location for seed ranges
+    # Q2 - get location for seed ranges (brute force)
     lowest_location = 10e50
     for start, end in zip(
         range(0, len(almanac["seeds"]), 2), range(2, len(almanac["seeds"]) + 1, 2)
@@ -95,3 +146,26 @@ if __name__ == "__main__":
             if location < lowest_location:
                 lowest_location = location
     print("Q2:", lowest_location)
+
+    # Q2 - get location for seed ranges (deduped seed ranges)
+    # lowest_location = 10e50
+    # print(almanac["seeds"])
+    # print(dedupe_seed_range(almanac["seeds"]))
+    # for start, end in zip(
+    #     range(0, len(almanac["seeds"]), 2), range(2, len(almanac["seeds"]) + 1, 2)
+    # ):
+    #     start_seed, length = almanac["seeds"][start:end]
+    #     for seed in range(start_seed, start_seed + length):
+    #         soil = get_destination(seed, almanac["seed-to-soil map"])
+    #         fert = get_destination(soil, almanac["soil-to-fertilizer map"])
+    #         water = get_destination(fert, almanac["fertilizer-to-water map"])
+    #         light = get_destination(water, almanac["water-to-light map"])
+    #         temp = get_destination(light, almanac["light-to-temperature map"])
+    #         humidity = get_destination(temp, almanac["temperature-to-humidity map"])
+    #         location = get_destination(humidity, almanac["humidity-to-location map"])
+    #         if location < lowest_location:
+    #             lowest_location = location
+    # print("Q2:", lowest_location)
+
+    # print(remove_overlap(range1=[10, 10], range2=[15, 10]))
+    # print(dedupe_seed_range([10, 10, 15, 10]))
---
+++ day8.py
@@ -0,0 +1,72 @@
+# --- Day 8: Haunted Wasteland ---
+# Level
+# Part 1: 22411
+# Part 2:
+
+from typing import List, Tuple
+import re
+
+
+def read_text_file(filepath: str) -> Tuple[str, dict]:
+    with open(filepath, "r") as f:
+        raw = [line.strip() for line in f.readlines()]
+    instructions = raw[0]
+
+    nodes = {}
+    for val in raw[2:]:
+        start_node, left_node, right_node = re.findall(r"[A-Z1-9]+", val)
+        nodes[start_node] = (left_node, right_node)
+    return (instructions, nodes)
+
+
+def step_generator(instructions):
+    yield from insructions
+
+
+if __name__ == "__main__":
+    # import data
+    insructions, nodes = read_text_file(filepath="data/day8.txt")
+
+    # Q1 - number of steps to ZZZ
+    current_node = "AAA"
+    step_counter = 0
+    step = step_generator(insructions)
+    i = 0
+    while current_node != "ZZZ":
+        try:
+            next_step = next(step)
+        except:
+            step = step_generator(insructions)
+            next_step = next(step)
+
+        if next_step == "L":
+            current_node = nodes[current_node][0]
+        else:
+            current_node = nodes[current_node][1]
+        step_counter += 1
+    print("Q1:", step_counter)
+
+    # Q2 - simulataneous steps from **A to **Z
+    current_node = [x for x in nodes.keys() if x.endswith("A")]  # all starting nodes
+    step_counter = 0
+    step = step_generator(insructions)
+    print(current_node)
+    not_converged = True
+    while not_converged:
+        try:
+            next_step = next(step)
+        except:
+            step = step_generator(insructions)
+            next_step = next(step)
+
+        if next_step == "L":
+            for i, node in enumerate(current_node):
+                current_node[i] = nodes[node][0]
+        else:
+            for i, node in enumerate(current_node):
+                current_node[i] = nodes[node][1]
+        step_counter += 1
+        if all([node[-1] == "Z" for node in current_node]):
+            not_converged = False
+        # print(next_step, current_node, [node[-1] == "Z" for node in current_node])
+    print("Q2:", step_counter)
