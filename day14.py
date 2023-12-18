b8a3fd971a48d33ce1f6dcd4a3192aafa65eed20
unknown <mosraughen@gmail.com>
2023-12-18 19:39:32 +1300

    day14 solutions

--- day14.py
+++ day14.py
@@ -5,7 +5,6 @@
 
 from typing import List, Tuple
 import re
-from tqdm import tqdm
 
 
 def read_text_file(filepath: str) -> List[str]:
@@ -71,50 +70,19 @@ def calc_total_load(data):
     return total_load
 
 
-def get_adjacent_coordinates(mapping: dict, coordinates_list: List[Tuple[int, int]]):
-    """Returns all adjacent coordinates for the input list of coordinates (excludes input coordinates)"""
-    adjacent_coordinates_list = []
-    for coordinates in coordinates_list:
-        x, y = coordinates
-        N = (x, y - 1)
-        E = (x + 1, y)
-        S = (x, y + 1)
-        W = (x - 1, y)
-        adjacent_coordinates_list.extend((N, NE, E, SE, S, SW, W, NW))
-
-    # keep valid coordinates, remove duplicates and original coordinates
-    adjacent_coordinates_list = [
-        x
-        for x in list(set(adjacent_coordinates_list))
-        if x in mapping.keys() and x not in coordinates_list
-    ]
-    return adjacent_coordinates_list
-
-
 if __name__ == "__main__":
-    data = read_text_file("data/day14_test.txt")
+    data = read_text_file("data/day14.txt")
 
     # Q1 - Total load after rollin north
     total_load = calc_total_load(tilt_platform(data, "N"))
     print("Total Load", total_load)
 
-    # Q2 - Total load after 1000000000 cycles cycle = (N,W, S, E)
-    # i = 0
-    # notConverged = True
-    # t0 = data
-    # while notConverged:
-    #     # cycle
-    #     t1 = tilt_platform(t0, "N")
-    #     t2 = tilt_platform(t1, "W")
-    #     t3 = tilt_platform(t2, "S")
-    #     t4 = tilt_platform(t3, "E")
-
-    #     if t4 == t0:  # cycle has converged
-    #         notConverged = False
-    #     i += 1
+    # Q2 - Total north load after 1000000000 cycles, 1 cycle = (N, W, S, E)
+    SPIN_CYCLES = 1000000000
     t0 = data
-    cycles = []
-    for i in tqdm(range(1000)):
+    loads = []  # each value is the data state after a full cycle
+    notConverged = True
+    while notConverged:
         # cycle
         t1 = tilt_platform(t0, "N")
         t2 = tilt_platform(t1, "W")
@@ -125,16 +93,28 @@ if __name__ == "__main__":
         l2 = calc_total_load(t2)
         l3 = calc_total_load(t3)
         l4 = calc_total_load(t4)
-
         t0 = t4
 
-        tmp = [str(l1), str(l2), str(l3), str(l4)]
-        cycles.append("_".join(tmp))
+        # check for loop
+        if t0 in loads:
+            num_cycles_before_loop_begins = loads.index(t0)
+            num_cycles_in_loop = len(loads) - num_cycles_before_loop_begins
+            notConverged = False
+        else:
+            loads.append(t0)
 
-        if i > 1000: # run for 1k cycles
-        if len(set(cycles)) == (0.5 * len(cycles)):
-            break
+    print(
+        "num_cycles_before_loop_begins:",
+        num_cycles_before_loop_begins,
+        "\nnum_cycles_in_loop:",
+        num_cycles_in_loop,
+    )
 
-    print(len(cycles))
-    print(cycles)
-    # print(cycles[-1])
+    # determine expected rock layout after 1000000000 cycles
+    id_at_spin_cycle = (
+        SPIN_CYCLES - num_cycles_before_loop_begins
+    ) % num_cycles_in_loop - 1
+    expected_data = loads[num_cycles_before_loop_begins + id_at_spin_cycle]
+
+    # expect load after 1000000000 cycles
+    print("Expected load at 1000000000 cycles:", calc_total_load(expected_data))
